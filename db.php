b8fea0f8d71f08900cd17ef163986282f2a9ce90
Fenz1t <144772234+Fenz1t@users.noreply.github.com>
2023-11-06 21:54:25 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php 
+
+$sName = "127.0.0.1";
+$uName = "root";
+$pass = "";
+$db_name = "to_do_list";
+
+try {
+    $conn = new PDO("mysql:host=$sName; dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
